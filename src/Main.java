0c48a8898ba5598e3ea108e76b30a13bb73c9317
Jakub Połeć <kubapo412@gmail.com>
2024-07-03 21:56:02 +0200

    Created Project

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("Hello and welcome!");
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
+
+        Main.test();
+    }
+
+
+
+    public static void test()
+    {
+        System.out.println("Test");
+    }
+
+
+}
